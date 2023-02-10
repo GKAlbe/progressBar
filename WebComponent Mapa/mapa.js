750ab9e25091cc463a09328d292e28d430f71d6b
GKAlbe <116301059+GKAlbe@users.noreply.github.com>
2023-02-10 19:27:39 +0000

    Add files via upload

--- WebComponent Mapa/mapa.js
+++ WebComponent Mapa/mapa.js
@@ -1,85 +1,112 @@
 class MiMapa extends HTMLElement {
 
-    constructor(width = 500, height = 400, precision = 1) {
-        super();
-        let widthM = this.getAttribute('my-width') || width;
-        let heightM = this.getAttribute('my-height') || height;
-        let precisionM = this.getAttribute('my-precision') || precision;
+  constructor(width = 500, height = 400, precision = 0.007, lat = 28.0198981, lon = -16.6438673) {
+      super();
+      let widthM = this.getAttribute('my-width') || width;
+      let heightM = this.getAttribute('my-height') || height;
+      let precisionM = this.getAttribute('my-precision') || precision;
+      this.lat = lat;
+      this.lon = lon;
+      this.createIframe(widthM, heightM);
+      this.posicionar(this.lat, this.lon, precisionM);
 
-        this.createIframe(widthM, heightM);
+      let p = document.createElement('p');
+      p.innerHTML = `
+        <button posicionar>Posicionar </button>
+      `;
+      this.append(p);
 
-       // if (navigator.geolocation) {
-            let lat =  navigator.geolocation.getCurrentPosition(this.showPositionLat);
-            let lon = navigator.geolocation.getCurrentPosition(this.showPositionLon);
-        //} else {
-            // lat = 28.0198981;
-            //  lon = -16.6438673;
-        //}
+     // if (navigator.geolocation) {
+      //} else {
+          // lat = 28.0198981;
+          //  lon = -16.6438673;
+      //}
 
-        this.posicionar(lat, lon, precisionM);
+      
 
-    }
-
-    createIframe(width, height) {
-        let iframe = document.createElement('iframe');
-        iframe.width = width;
-        iframe.height = height;
-        iframe.src = '';
-        iframe.classList.add('iframe');
-        this.append(iframe);
-    }
-
-    posicionar(lat, lon, precision) {
-        let iframe = this.querySelector('iframe');
-             iframe.src = `https://www.openstreetmap.org/export/embed.html?bbox=${lon-precision},${lat-precision},${lon+precision},${lat+precision}&layer=mapnik`;
-         }
-        
- 
-    
-
-    //  getLocation() {
-    //     if(navigator.geolocation) {
-    //        pos =  navigator.geolocation.getCurrentPosition;
-    //         lat = navigator.geolocation.getCurrentPosition.coords.latitude;
-    //         lon = pos.coords.longitude;
-    //     } else {
-    //         lat = 28.0198981;
-    //         lon = -16.6438673;
-    //     }
-
-    //  }
-      showPositionLat(position) {
-        // console.log(position.coords.latitude);
-        return position.coords.latitude ; 
-         
-       }
-
-        showPositionLon(position) {
-        return position.coords.longitude;
-       }
-
-
-}
-
-function getLocation() {
-    if(navigator.geolocation) {
-       navigator.geolocation.getCurrentPosition(showPositionLat);
-       navigator.geolocation.getCurrentPosition(showPositionLat);
-      //  lat = navigator.geolocation.getCurrentPosition.coords.latitude;
-       // lon = pos.coords.longitude;
-    } else {
-        console.log('xd');
-    }
-}
-
-function showPositionLat(position) {
-   // console.log(position.coords.latitude);
-   return position.coords.latitude ; 
-    
   }
 
-function showPositionLon(position) {
-    return position.coords.longitude;
+  connectetdCallback() {
+    this.querySelector('button[posicionar]').addEventListener('click', this.getLocation);
+  }
+
+  disconnectedCallback() {
+    this.querySelector('button[posicionar]').removeEventListener('click', this.getLocation);
+ 
+  }
+
+  createIframe(width, height) {
+      let iframe = document.createElement('iframe');
+      iframe.width = width;
+      iframe.height = height;
+      iframe.src = '';
+      iframe.classList.add('iframe');
+      this.append(iframe);
+  }
+
+  posicionar(lat, lon, precision) {
+    console.log('hola');
+      let iframe = this.querySelector('iframe');
+      
+           iframe.src = `https://www.openstreetmap.org/export/embed.html?bbox=${lon-precision},${lat-precision},${lon+precision},${lat+precision}&layer=mapnik`;
+       }
+
+      getLocation() {
+        console.log('hola');
+        if (this.navigator.geolocation) {
+          this.navigator.geolocation.getCurrentPosition(this.showPosition);
+        } else { 
+          x.innerHTML = "Geolocation is not supported by this browser.";
+        }
+      }
+
+       showPosition(position) {
+        let iframe = this.querySelector('iframe');
+        this.lat = position.coords.latitude;
+        this.lon = position.coords.longitude;
+        iframe.src = `https://www.openstreetmap.org/export/embed.html?bbox=${position.coords.longitude-this.precision},${position.coords.latitude-this.precision},${position.coords.longitude+this.precision},${position.coords.latitude+this.precision}&layer=mapnik`;
+       // x.innerHTML = "Latitude: " + position.coords.latitude + 
+       // "<br>Longitude: " + position.coords.longitude;
+      }
+
+
+      static get observedAttributes() {
+        return ['lat', 'lon', 'precision'];
+      }
+
    }
+
+
+
+
+function getLocation() {
+  if(navigator.geolocation) {
+     navigator.geolocation.getCurrentPosition(showPositionLat);
+     navigator.geolocation.getCurrentPosition(showPositionLon);
+    //  lat = navigator.geolocation.getCurrentPosition.coords.latitude;
+     // lon = pos.coords.longitude;
+  } else {
+      console.log('xd');
+  }
+}
+
+function showPosition(position) {
+  let iframe = this.querySelector('iframe');
+  this.lat = position.coords.latitude;
+  this.lon = position.coords.longitude;
+  iframe.src = `https://www.openstreetmap.org/export/embed.html?bbox=${position.coords.longitude-this.precision},${position.coords.latitude-this.precision},${position.coords.longitude+this.precision},${position.coords.latitude+this.precision}&layer=mapnik`;
+ // x.innerHTML = "Latitude: " + position.coords.latitude + 
+ // "<br>Longitude: " + position.coords.longitude;
+}
+
+// function showPositionLat(position) {
+//  // console.log(position.coords.latitude);
+//  return position.coords.latitude ; 
+  
+// }
+
+// function showPositionLon(position) {
+//   return position.coords.longitude;
+//  }
 
 customElements.define('mi-mapa', MiMapa);
